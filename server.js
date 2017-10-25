7723ec44cd84449f5c8317785b395bf9c1487110
Bill Sorrentino <williamsorrentino@gmail.com>
2017-10-25 19:35:02 -0400

    app is a mess

--- server.js
+++ server.js
@@ -1,87 +1,56 @@
 // setting up the dependencies for the app//
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var hbs =require('hbs');
-var fs = ('fs');
+
+var express = require("express");
+var bodyParser = require("body-parser");
+//var express = require('express');
+//var bodyParser = require('body-parser');
+//var path = require('path');
+//var hbs =require('hbs');
+//var fs = ('fs');
 
 
 //create and express server and set up the port
-var port = process.env.Port || 3000;
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 
-////set up handlebars partials
-//hbs.registerPartials(__dirname + '/views/partials');
-
-app.set('view engine', 'hbs');
-//app.set('view engine', 'html');
-//app.engine('html', require('ejs').renderFile);
-
-app.use((req, res, next) => {
-    var now = new Date().toString();
-    var log = `${now}: ${req.method} ${req.url}`;
-    
-    console.log(log);
-   
-   
- next();
-});
-
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear()
-});
-
-hbs.registerHelper('screamIt', (text) => {
-    return text.toUpperCase();
-});
-
-app.use(express.static(__dirname + './public'));
-
-
-// friend finder page
-app.get('/', function (req, res) {
-    res.render('friendfinder.hbs', {
-        pageTitle: 'Friend Finder',
-        welcomeMessage: 'Find your new best friend'
-    });
-});
-
-app.get('/survey', function (req, res) {
-    res.render('survey.hbs', {
-        pageTitle: 'About Page',
-
-    });
-});
 
 //Body Parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
+//app.set('view engine', 'html');
 
-
-//Router
-
-//app.get('/views/routing/api-routes.js');
-//app.get('/app/routing/html-routes.js');
-
-require('/views/routing/api-routes.js')(app);
-require('/app/routing/html-routes.js')(app);
+//app.use(express.static('app/public'));
 
 
 
-//console.log(error);
+//app.get('/friendfinder', function (req, res) {
+//    res.render('friendfinder', {
+//        pageTitle: 'Friend Finder',
+//        welcomeMessage: 'Find your new best friend'
+//    });
+//});
+//
+//app.get('/survey', function (req, res) {
+//    res.render('survey.html', {
+//        pageTitle: 'About Page',
+//
+//    });
+//});
+
+
+
+require("./app/routes/apiRoutes.js") (app);
+require("./app/routes/htmlRoutes.js") (app);
 
 
 
 
-
-
-
-app.listen(port, function() {
-    console.log(`Server is up on port ${port}`);
+app.listen(PORT, function() {
+    console.log(`Server is up on port ${PORT}`);
     
 });
